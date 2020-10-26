e990683a189031e1a53279b9a9106437713bb4dc
cljaray <cl.jaray@gmail.com>
2020-10-26 18:49:17 -0300

    AOS agregado

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
     */
+
+    AOS.init();
     (function ($) {
     "use strict"; // Start of use strict
 
